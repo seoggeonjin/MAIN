e2952a9e7fba44e210d2dc5ca06b898c77bccfa7
seoggeonjin <119400808+seoggeonjin@users.noreply.github.com>
2022-12-01 19:06:31 +0900

    Create helps.py

---
+++ xyz/helps.py
@@ -0,0 +1,11 @@
+def helps():
+    print(r"xyz/ ")
+    print(">>> from xyz.xyz import p.mfp\n>>> p.px(\"\")\n>>> p.pxy(\"\")\n>>> p.xyz(\"\")")
+    st = str()
+    for i in range(25):
+        st += "-"
+    print(st)
+    print(r"cal/ ")
+    print(">>> from xyz.cal import mainf\n>>> mainf.run()")
+    print(st)
+    print("etc...")
